babde30b370e267644bf38012f8fadce3a3310e4
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2022-05-30 08:53:55 -0700

    Made sure it ignored files that start with a dot

--- test/test.d
+++ test/test.d
@@ -8,8 +8,8 @@
 unittest {
 	import BDD;
 	import helpers;
-	import pack;
-	import unpack;
+	import pack : packDir;
+	import unpack: unpackDir;
 	import std.file : dirEntries, SpanMode, isDir, isFile, remove, rmdirRecurse, exists, mkdir, getSize;
 
 	describe("make-binaries-7z",
@@ -47,6 +47,7 @@ unittest {
 				rmdirRecurse("temp_test_files");
 			}
 		}),
+
 		it("Should round trip files", delegate() {
 			ulong original_size = "test_data/aaa/bbb/ccc.zip".getSize;
 
@@ -54,10 +55,12 @@ unittest {
 			packDir(".", true);
 			"test_data/aaa/bbb/ccc.zip.zip.smol".exists.shouldEqual(true);
 			"test_data/aaa/bbb/ccc.zip".exists.shouldEqual(false);
+			"test_data/aaa/bbb/.ignore_this_dir/blah.zip".exists.shouldEqual(true);
 
 			unpackDir(".", true);
 			"test_data/aaa/bbb/ccc.zip".exists.shouldEqual(true);
 			"test_data/aaa/bbb/ccc.zip.zip.smol".exists.shouldEqual(false);
+			"test_data/aaa/bbb/.ignore_this_dir/blah.zip".exists.shouldEqual(true);
 
 			//
 			ulong new_size = "test_data/aaa/bbb/ccc.zip".getSize;
@@ -67,7 +70,7 @@ unittest {
 			// Recompress the test dir files
 			packDir(".", true);
 
-			// Make sure the files have been recompressed
+			// Make sure the files have been compressed
 			"test_data/aaa".exists.shouldEqual(true);
 			"test_data/aaa/bbb".exists.shouldEqual(true);
 			"test_data/aaa/bbb/ccc.zip.zip.smol".exists.shouldEqual(true);
@@ -84,6 +87,13 @@ unittest {
 			"test_data/aaa/formats/zzz.txt.gz.gzip.smol".exists.shouldEqual(true);
 			"test_data/aaa/formats/zzz.txt.xz".exists.shouldEqual(true);
 			"test_data/aaa/formats/zzz.zip.zip.smol".exists.shouldEqual(true);
+
+			// Make sure entries starting with a dot have been ignored
+			"test_data/aaa/bbb/.ignore_this_dir".exists.shouldEqual(true);
+			"test_data/aaa/bbb/.ignore_this_dir/blah.txt".exists.shouldEqual(true);
+			"test_data/aaa/bbb/.ignore_this_dir/blah.zip".exists.shouldEqual(true);
+			"test_data/aaa/bbb/.ignore_this_file.txt".exists.shouldEqual(true);
+			"test_data/aaa/bbb/.ignore_this_file.zip".exists.shouldEqual(true);
 		}),
 	);
 }
